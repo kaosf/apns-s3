cf1d0b1bbcaee05cf1f48440e329c3a6cf618cf0
ka <ka.kaosf@gmail.com>
2016-12-31 03:51:18 +0900

    Set SimpleCov configuration only on the development (NOT CI) environments

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,11 +4,12 @@ require 'test/unit'
 require 'test/unit/rr'
 
 Coveralls.wear!
-SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
-SimpleCov.start
+if ENV['CI'] != 'true'
+  SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+  SimpleCov.start
+end
 
 # require order ref. http://qiita.com/bsdmad/items/ab8b7d92d965df8bb2d8
-
 # ref.
 #   https://coveralls.zendesk.com/hc/en-us/articles/201769485-Ruby-Rails
 #   http://stackoverflow.com/questions/12983137/how-do-detect-if-travis-ci-or-not
