e5278ccb41225be7629ada7f9ecc51c27f893219
ka <ka.kaosf@gmail.com>
2016-12-31 01:58:00 +0900

    Implement the tests
    ref.
      http://qiita.com/repeatedly/items/727b08599d87af7fa671
      http://tmtms.hatenablog.com/entry/2014/10/12/ruby-test-unit
      https://test-unit.github.io/ja/#test-unit-rr
      http://qiita.com/yshr04hrk/items/a47d52a49f2159ff3cef
      https://github.com/rr/rr
      http://www.rubydoc.info/gems/test-unit-rr/Test/Unit/RR/Adapter#assert_rr-instance_method

--- test/test_apns-s3.rb
+++ test/test_apns-s3.rb
@@ -1,9 +1,28 @@
 require 'test/unit'
+require 'test/unit/rr'
 require 'apns-s3'
 
 class TestApnsS3 < Test::Unit::TestCase
-  # TODO implement
-  test "#set_pemfile" do
-    assert true
+  setup do
+    @pem_filename = 'dummy.pem'
+  end
+
+  test "pemfile exists" do
+    stub(File).exists?(@pem_filename) { true }
+    mock(APNS).pem=(@pem_filename)
+    ApnsS3.set_pemfile filename: @pem_filename
+  end
+
+  test "pemfile DOES NOT exist" do
+    stub(File).exists?(@pem_filename) { false }
+    credentials_stub = Object.new
+    stub(Aws::Credentials).new { credentials_stub }
+    region = 'dummy-region-name'
+    stub(Aws.config).update(region: region, credentials: credentials_stub)
+    client_stub = Object.new
+    stub(Aws::S3::Client).new { client_stub }
+    stub(client_stub).get_object
+    mock(APNS).pem=(@pem_filename)
+    ApnsS3.set_pemfile region: region, filename: @pem_filename
   end
 end
