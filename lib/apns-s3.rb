58282e2fc9aa1b7481c5932506de4bb36bfbe3dd
ka <ka.kaosf@gmail.com>
2017-03-04 11:31:31 +0900

    Revert the commit that modifies to pemfile_exist? method

--- lib/apns-s3.rb
+++ lib/apns-s3.rb
@@ -24,7 +24,7 @@ module ApnsS3
     bucketname: nil,
     filename: nil
   )
-    unless pemfile_exist? filename
+    unless File.exist? filename
       credentials = Aws::Credentials.new(
         aws_access_key_id, aws_secret_access_key
       )
@@ -33,9 +33,5 @@ module ApnsS3
       s3.get_object response_target: filename, bucket: bucketname, key: filename
     end
     APNS.pem = filename
-  end
-
-  def self.pemfile_exist? filename
-    File.exist? filename
   end
 end
