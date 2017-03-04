a52705c79abd69eccd8fe2a77b04419e950fc755
ka <ka.kaosf@gmail.com>
2017-03-04 11:24:07 +0900

    Fix the test with fakefs

--- test/test_apns-s3.rb
+++ test/test_apns-s3.rb
@@ -8,21 +8,29 @@ class TestApnsS3 < Test::Unit::TestCase
   end
 
   test "pemfile exists" do
-    stub(ApnsS3).pemfile_exist?(@pem_filename) { true }
+    # FakeFS do
+    #   FileUtils.touch @pem_filename
+    #   mock(APNS).pem=(@pem_filename)
+    #   ApnsS3.set_pemfile filename: @pem_filename
+    # end
+    #
+    # # NOTE: this code makes a warning below:
+    # # .../vendor/bundle/ruby/2.4.0/gems/fakefs-0.10.2/lib/fakefs/base.rb:21:warning: instance variable @activated not initialized
+
+    FakeFS.activate!
+    FileUtils.touch @pem_filename
     mock(APNS).pem=(@pem_filename)
     ApnsS3.set_pemfile filename: @pem_filename
+    FakeFS.deactivate!
   end
 
   test "pemfile DOES NOT exist" do
-    stub(ApnsS3).pemfile_exist?(@pem_filename) { false }
-    credentials_stub = Object.new
-    stub(Aws::Credentials).new { credentials_stub }
-    region = 'dummy-region-name'
-    stub(Aws.config).update(region: region, credentials: credentials_stub)
+    assert !File.exist?(@pem_filename)
+
     client_stub = Object.new
     stub(Aws::S3::Client).new { client_stub }
     stub(client_stub).get_object
     mock(APNS).pem=(@pem_filename)
-    ApnsS3.set_pemfile region: region, filename: @pem_filename
+    ApnsS3.set_pemfile region: 'fake-region', filename: @pem_filename
   end
 end
